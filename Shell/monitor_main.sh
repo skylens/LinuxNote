6f139920f50ed396c1ed4277971d17261b18efa9
skylens <skylens116@outlook.com>
2016-09-08 20:38:06 +0800

    9-8-02

--- Shell/monitor_main.sh
+++ Shell/monitor_main.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+resettem=$(tput sgr0)
+declare -A ssharray
+i=0
+numbers=""
+for script_file in `ls -I "monitor_main.sh" ./`
+do
+	echo -e "\e[1;35m" "The Script:"${i} '==>' ${resettem} ${script_file}
+  	grep -E "^\#Program function" ${script_file}
+  	ssharray[$i]=${script_file}
+  	numbers="${numbers} | ${i}"
+  	i=$((i+1))
+done
+while true
+do
+	read -p "Please input a number [ ${numbers} ]:" execshell
+	if [[ ! ${execshell} =~ ^[0-9]+ ]];then
+		exit 0
+	fi
+	/bin/bash ./${ssharray[$execshell]}
+done
--- Shell/system_monitor.sh
+++ Shell/system_monitor.sh
@@ -1,1 +1,38 @@
 #!/bin/bash
+clear
+
+if [ $# -eq 0 ] ;then
+# Define variable reset_terminal
+reset_terminal=$(tput sgr0)
+#Check OS Type
+	os=$(uname -o)
+	echo -e '\E[32m' "Check OS Type:" $reset_terminal $os
+#Check OS Release Version and Name
+	os_name=$(cat /etc/issue|grep -e "Server")
+	echo -e '\E[32m' "Check OS Release Version and Name:" $reset_terminal $os_name
+#Check Architecture
+	architecture=$(uname -m)
+	echo -e '\E[32m' "Check Architecture:" $reset_terminal $architecture
+#Check Kernel Release
+	kernelrelease=$(uname -r)
+	echo -e '\E[32m' "Check Kernel Release:" $reset_terminal $kernelrelease
+#Check hostname
+	#echo $HOSTNAME
+	hostname=$(uname -n)
+	echo -e '\E[32m' "Check hostname:" $reset_terminal $hostname
+#Check if connected to Internet or not
+	ping -c 3 www.163.com &>/dev/null && echo "Internet:Conneted" || echo "Internet:Disconnected"
+#Check Internal IP
+  internalip=$(hostname -I)
+  echo -e '\E[32m' "Check Internal IP:" $reset_terminal $internalip
+#Check External IP
+  externalip=$(curl -s http://ipecho.net/plain)
+  echo -e '\E[32m' "Check External IP:" $reset_terminal $externalip
+#Check DNS
+  nameservers=$(cat /etc/resolv.conf |grep -E "\<nameserver[ ]+"|awk '{print $NF}')
+  echo -e '\E[32m' "Check DNS:" $reset_terminal $nameservers
+#Check Logged In Users
+  who>/tmp/who
+  echo -e '\E[32m' "Check Logged In Users:" $reset_terminal && cat /tmp/who
+  rm -rf /tmp/who
+fi
