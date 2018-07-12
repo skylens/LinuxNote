ccb2ee0bb8760bff4e6c5db0888946bcaaf0520c
skylens <skylens116@outlook.com>
2018-07-12 22:27:32 +0800

    update

--- bsd/freebsd.sh
+++ bsd/freebsd.sh
@@ -11,6 +11,8 @@ cd /usr/ports/dns/udns
 make install clean
 cd /usr/ports/devel/libev
 make install clean
+cd /usr/ports/devel/pcre
+make install clean
 wget https://github.com/shadowsocks/shadowsocks-libev/releases/download/v3.0.8/shadowsocks-libev-3.0.8.tar.gz
 cd shadowsocks-libev-3.0.8
 ./configure --disable-documentation
